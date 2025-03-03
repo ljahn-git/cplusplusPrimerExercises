e4bd327a7929de29a898327f2b9ea1d9b452cec5
ljahn-git <ljahn724@gmail.com>
2025-03-02 21:08:12 -0500

    Completed up to 1.19

--- chapter1.cc
+++ chapter1.cc
@@ -9,10 +9,11 @@ int main() {
     //Apparently nothing different on macos
 
     //Exercise 1.3
-    std::cout << "\nExercise 1.3:" << std::endl;
-    std::cout << "Hello World!" << std::endl;
-
+    //std::cout << "\nExercise 1.3:" << std::endl;
+    //std::cout << "Hello World!" << std::endl;
+    
     //Exercise 1.4 & 1.5
+    /*
     std::cout << "\nExercise 1.4 & 1.5:" << std::endl;
     int a, b;
     std::cin >> a >> b;
@@ -23,6 +24,7 @@ int main() {
     std::cout << " is ";
     std::cout << a * b;
     std::cout << std::endl;
+    */
     
 
     //Exercise 1.6
@@ -35,29 +37,36 @@ int main() {
    //Incorrectly nested comments sure do not compile yup
 
    //Exercise 1.8
-   std::cout << "\nExercise 1.7:" << std::endl;
-   std::cout << "/*" << std::endl; //compiles
-   std::cout << "*/" << std::endl; //compiles
-   std::cout << /* "*/" " /* */ << std::endl; //compiles
-   std::cout << /* "*/" /* "/*" */ << std::endl; //compiles
+   
+   //std::cout << "\nExercise 1.7:" << std::endl;
+   //std::cout << "/*" << std::endl; //compiles
+   //std::cout << "*/" << std::endl; //compiles
+   //std::cout << /* "*/" " /* */ << std::endl; //compiles
+   //std::cout << /* "*/" /* "/*" */ << std::endl; //compiles
+   
 
    //Exercise 1.9: Write a program that uses a while to sum the numbers from 50 to 100
+   /*
    std::cout << "\nExercise 1.9:" << std::endl;
    int x = 50;
     while (x <= 100) {
         std::cout << x << std::endl;
         x++;
     }
+    */
 
    //Exercise 1.10: Use the decrement operator to write a while that prints the numbers from 10 down to 0
+   /*
     std::cout << "\nExercise 1.10:" << std::endl;
     int y = 10;
     while (y >= 0) {
         std::cout << y << std::endl;
         y--;
     }
+    */
 
    //Exercise 1.11: Write a program that prompts the user for two integers. Print each number in the range specified by those two integers.
+   /*
     std::cout << "\nExercise 1.11:" << std::endl;
     int first, second;
 
@@ -67,11 +76,13 @@ int main() {
         std::cout << first << std::endl;
         first++;
     }
+    */
 
     //Exercise 1.12
     //The given for loop adds the numbers between -100 and 100 to sum. The final value of sum will be 0.
 
     //Exercise 1.13: Rewrite the first two exercises from p. 13 using for loops.
+    /*
     std::cout << "\nExercise 1.13:" << std::endl;
     for (int i = 50; i <= 100; i++) {
         std::cout << i << std::endl;
@@ -79,6 +90,8 @@ int main() {
     for (int i = 10; i >= 0; i--) {
         std::cout << i << std::endl;
     }
+    */
+
     //Exercise 1.14: Compare for loops and while loops.
     //For loops contain a localized variable that allows them to specialize in repeating for n amount of times.
     //While loops are better for repeating until a certain condition is met.
@@ -89,5 +102,56 @@ int main() {
     //char t = temp;
     //std::cout << undeclared;
 
+    //Exercise 1.16: Write your own version of a program that prints the sum of a set of integers read from cin
+    /*
+    std::cout << "\nExercise 1.16:" << std::endl;
+    int sum = 0, value = 0;
+    while (std::cin >> value) {
+        sum += value;
+    }
+    std::cout << sum << std::endl;
     return 0;
+    */
+
+   //Exercise 1.17 & 1.18:
+   //If the input values are the same, then the count for that number is incremented
+   //If there are no duplicate values, then each number will be said to occur only one time
+   /*
+   std::cout << "\nExercise 1.17 & 1.18:" << std::endl;
+   int currVal = 0, val = 0;
+   if (std::cin >> currVal) {
+        int cnt = 1;
+        while (std::cin >> val) {
+            if (val == currVal) {
+                cnt++;
+            } else {
+                std::cout << currVal << " occurs "
+                << cnt << " times" << std::endl;
+                currVal = val;
+                cnt = 1;
+            }
+        }
+        std::cout << currVal << " occurs "
+        << cnt << " times" << std::endl;
+   }
+   */
+
+    //Exercise 1.19: Rewrite 1.11 so it works no matter the order of the inputs
+    /*
+    std::cout << "\nExercise 1.19:" << std::endl;
+    int first, second;
+    std::cout << "Enter two integers: ";
+    std::cin >> first >> second;
+
+    if (first > second) {
+        int temp = first;
+        first = second;
+        second = temp;
+    }
+
+    while (first <= second) {
+        std::cout << first << std::endl;
+        first++;
+    }
+    */
 }
